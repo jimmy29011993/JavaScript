50ede4a427be8e51860dc6a832d68c55ea03fde7
Jimmy Maldonado <jimmy29011993@gmail.com>
2015-03-06 17:03:26 -0400

    Upload main file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 /**
  * Created by Jimmy Maldonado on 3/6/2015.
  */
-console.error('mensaje de error');
+window.onload=
+function Myfunction() {
+    var name = window.prompt('What is your name');
+    var edad = window.prompt('How old are you');
+    if(edad > 50)
+        window.alert('You are an old person');
+    else
+        window.alert('You are a young person');
+};
